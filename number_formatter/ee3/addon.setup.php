f278e2bd9c47a4d513260258a03ab2e47710d829
Doug Green <doug@dsgreen.com>
2017-02-22 17:27:11 -0500

    Add preliminary functional EE3 version

---
+++ number_formatter/ee3/addon.setup.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'author'      => 'Douglas Green',
+    'author_url'  => 'http://greeninteractive.com',
+    'name'        => 'Number Formatter',
+    'description' => 'Sends a value through the PHP number_format() function, and returns a formatted number.',
+    'version'     => '2.0',
+    'namespace'   => 'GreenInteractive\Number_formatter'
+);
